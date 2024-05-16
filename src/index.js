092743c05bb8f7f6e973ce305406258af2f0dcd5
VasuGolakiya <vasugolakiya2344@gmail.com>
2024-05-16 21:16:46 +0530

    Adding utils

--- src/index.js
+++ src/index.js
@@ -5,4 +5,16 @@ dotenv.config({
     path: './env'
 });
 
-connectDB()
+connectDB();
+.then(()=>{
+    app.on("error",(err)=>{
+        console.log("Error: ",err);
+        throw err;
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed!",err);
+})
